875a1a96385b6ebee3a1b36c7ad462639c0dc40c
Emile <emuller2607@gmail.com>
2025-08-14 13:50:54 +0200

    Cleanup.

--- src/main/java/com/emcode/aikinetics/AiKineticsApplication.java
+++ src/main/java/com/emcode/aikinetics/AiKineticsApplication.java
@@ -1,7 +1,5 @@
 package com.emcode.aikinetics;
 
-import com.emcode.aikinetics.model.Account;
-import com.emcode.aikinetics.repository.AccountRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/emcode/aikinetics/api/error/ErrorResponse.java
+++ src/main/java/com/emcode/aikinetics/api/error/ErrorResponse.java
@@ -1,10 +1,4 @@
 package com.emcode.aikinetics.api.error;
 
-public  class ErrorResponse {
-    public final String code;
-    public final String message;
-    public ErrorResponse(String code, String message) {
-        this.code = code;
-        this.message = message;
-    }
+public record ErrorResponse(String code, String message) {
 }
