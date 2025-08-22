af7517234ecbc49c69e6c078f392ca3c633e0fec
Emile <emuller2607@gmail.com>
2025-08-22 14:29:21 +0200

    Add unique check and DuplicateFieldException.

---
+++ src/main/java/com/emcode/aikinetics/api/error/DuplicateFieldException.java
@@ -0,0 +1,7 @@
+package com.emcode.aikinetics.api.error;
+
+public class DuplicateFieldException extends RuntimeException {
+    public DuplicateFieldException(String message) {
+        super(message);
+    }
+}
